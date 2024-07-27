7798402965976e3d3eaa0f70c97e649622349b0a
hennrry <hennrrycanahuiholasoy@gmail.com>
2024-07-27 10:49:49 -0600

    se le añadio el menu

--- untitled/src/Menu.java
+++ untitled/src/Menu.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
 public class Menu {
+    public void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("----- Menú Principal -----");
+            System.out.println("1. Laptop");
+            System.out.println("2. Desktop");
+            System.out.println("3. Smartphone");
+            System.out.println("4. Tablet");
+            System.out.println("5. Refrigerador");
+            System.out.println("7. Salir");
+            System.out.print("Elige una opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    //
+                    break;
+                case 2:
+                   //
+                    break;
+                case 3:
+                    //
+                    break;
+                case 4:
+                    //
+                    break;
+                case 5:
+                    //
+                    break;
+                case 6:
+                    //
+                    break;
+                case 7:
+                    System.out.println("Saliendo del programa...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+            }
+        }
+    }
+
 }
